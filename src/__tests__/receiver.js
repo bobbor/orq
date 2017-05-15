119babad6d9e7121ce1aac07fddfd7681b7d6169
Luca Nils Schmid <me@lucaschmid.net>
2017-05-15 17:48:05 +0200

    feat(receiver): implement module

---
+++ src/__tests__/receiver.js
@@ -0,0 +1,137 @@
+// @flow
+
+import test from 'ava'
+import { Observable as O } from 'rxjs'
+
+import { mockWorker, mockCache } from './helpers'
+import {
+  msg,
+  isResponseMsg,
+  REQUEST,
+  UNSUBSCRIBE,
+  RESPONSE_NOTIFICATION,
+  CLEAR_CACHE,
+  INVALIDATE,
+} from '../lib/message'
+import mkReceiver from '../receiver'
+
+import type { Cache } from '../cache'
+
+test.cb('should listen for REQUEST messages from worker and query the cache.', t => {
+  const response = 'Cached Response'
+  const url = `https://example.com`
+  const addRequestMock = () => O.of(response)
+  const cacheMock = mockCache({
+    get: (key: string) => {
+      t.is(key, `GET:${url}`)
+      t.end()
+      return O.of('Cached Response')
+    }
+  })
+  const workerMock = mockWorker()
+
+  mkReceiver(workerMock, addRequestMock, cacheMock)
+  workerMock.postMessage(msg(REQUEST, { url }), true)
+})
+
+test.cb('should cancel the request when an UNSUBSCRIBE message is received', t => {
+  const response = 'Cached Response'
+  const url = `https://example.com`
+  const addRequestMock = () => O.create((o) => () => {
+    t.pass()
+    t.end()
+  })
+  const cacheMock = mockCache({})
+  const workerMock = mockWorker()
+
+  mkReceiver(workerMock, addRequestMock, cacheMock)
+  const requestMessage = msg(REQUEST, { url })
+  workerMock.postMessage(requestMessage, true)
+  setTimeout(() => {
+    workerMock.postMessage(msg(UNSUBSCRIBE, undefined, requestMessage.id), true)
+  })
+})
+
+test.cb('should call `addRequest` if there is a cache miss.', t => {
+  const response = 'Requested Response'
+  const url = `https://example.com`
+  const addRequestMock = () => {
+    t.pass()
+    t.end()
+    return O.of(response)
+  }
+  const cacheMock = mockCache({})
+  const workerMock = mockWorker()
+
+  mkReceiver(workerMock, addRequestMock, cacheMock)
+  const requestMessage = msg(REQUEST, { url })
+  workerMock.postMessage(requestMessage, true)
+})
+
+test.cb('should send the response back to the worker with the request id and the type RESPONSE_NOTIFICATION', t => {
+  const response = 'Requested Response'
+  const url = `https://example.com`
+  const addRequestMock = () => O.of(response)
+  const cacheMock = mockCache({})
+  const requestMessage = msg(REQUEST, { url })
+  const workerMock = mockWorker((message, postMessage) => {
+    if (!isResponseMsg(requestMessage)(message)) {
+      return postMessage(message)
+    }
+    t.is(message.id, requestMessage.id)
+    t.is(message.payload.value, response)
+    t.end()
+  })
+
+  mkReceiver(workerMock, addRequestMock, cacheMock)
+  workerMock.postMessage(requestMessage, true)
+})
+
+test.cb('should insert the response from `addRequest` into the cache.', t => {
+  const response = 'Requested Response'
+  const url = `https://example.com`
+  const addRequestMock = () => O.of(response)
+  const cacheMock = mockCache({
+    set: (key: string, value: string) => {
+      t.is(value, response)
+      t.end()
+      return O.of(value)
+    }
+  })
+  const workerMock = mockWorker()
+
+  mkReceiver(workerMock, addRequestMock, cacheMock)
+  const requestMessage = msg(REQUEST, { url })
+  workerMock.postMessage(requestMessage, true)
+})
+
+test.cb('should call `clear` on the cache if it receives a CLEAR_CACHE message.', t => {
+  const addRequestMock = () => O.of(true)
+  const cacheMock = mockCache({
+    clear: () => {
+      t.pass()
+      t.end()
+      return O.empty()
+    }
+  })
+  const workerMock = mockWorker()
+
+  mkReceiver(workerMock, addRequestMock, cacheMock)
+  workerMock.postMessage(msg(CLEAR_CACHE), true)
+})
+
+test.cb('should `delete` a given item if it receives a INVALIDATE message.', t => {
+  const url = `https://example.com`
+  const addRequestMock = () => O.of(true)
+  const cacheMock = mockCache({
+    delete: (key: string) => {
+      t.is(key, `GET:${url}`)
+      t.end()
+      return O.empty()
+    }
+  })
+  const workerMock = mockWorker()
+
+  mkReceiver(workerMock, addRequestMock, cacheMock)
+  workerMock.postMessage(msg(INVALIDATE, { url }), true)
+})
---
+++ src/receiver.js
@@ -0,0 +1,99 @@
+// @flow
+
+import Rx, { Observable as O, Notification } from 'rxjs'
+
+import {
+  msg,
+  isMsg,
+  REQUEST,
+  UNSUBSCRIBE,
+  RESPONSE_NOTIFICATION,
+  CLEAR_CACHE,
+  INVALIDATE,
+} from './lib/message'
+
+import type { Cache } from './cache'
+import type { RequestOptions } from './request'
+
+type AddRequest = (
+  url: string,
+  options?: RequestOptions
+) => Rx.Observable<any>
+
+const mkRequestQueueReceiver = <Res>(
+  worker: Worker,
+  addRequest: AddRequest,
+  cache: Cache,
+): void => {
+  O.fromEvent(worker, 'message')
+    .pluck('data')
+    .filter(isMsg)
+    .groupBy(({ id }) => id)
+    .subscribe(
+      message$ => {
+        // FIXME: this is fucked. find a better way!
+        let message
+        let testSub
+
+        const sub = message$
+          .do(tmpMessage => {
+            message = tmpMessage
+          })
+          .do(() => {
+            if (message.type === UNSUBSCRIBE) {
+              sub.unsubscribe()
+            }
+          })
+          .concatMap(() => {
+            const { type, id, payload } = message
+            const options = Object.assign(
+              (payload && payload.options) || {},
+              { method: 'GET' }
+            )
+            if (type === REQUEST) {
+              const { url } = payload
+              if (options.method === 'GET') {
+                const key = genCacheKey(url, options)
+                return cache.get(key)
+                  .concatMap(cachedValue =>
+                    cachedValue
+                      ? O.of(cachedValue)
+                      : addRequest(url, options)
+                        .concatMap(newValue =>
+                          cache.set(key, newValue)
+                        )
+                  )
+              } else {
+                return addRequest(url, options)
+              }
+            } else if (type === INVALIDATE) {
+              const { url } = payload
+              const key = genCacheKey(url, options)
+              return cache.delete(key)
+            } else if (type === CLEAR_CACHE) {
+              return cache.clear()
+            } else {
+              return O.empty()
+            }
+          })
+          .materialize()
+          .subscribe(
+            notification => {
+              const { type, id } = message
+              worker.postMessage(msg(
+                type,
+                notification,
+                id
+              ))
+            },
+            error => { console.error(error) }
+          )
+      },
+      error => { console.error(error) }
+    )
+}
+
+const genCacheKey = (url: string, options: RequestOptions) =>
+  `${options.method || 'GET'}:${url}`
+
+export default mkRequestQueueReceiver
