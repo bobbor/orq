b61ee43724f628d271f9bbef74ef1209819269c9
Luca Nils Schmid <me@lucaschmid.net>
2017-05-17 10:28:41 +0200

    feat(mem-cache): implement module

---
+++ src/__tests__/mem-chache.js
@@ -0,0 +1,68 @@
+// @flow
+
+import test from 'ava'
+import { Observable as O } from 'rxjs'
+
+import type { Cache } from '../cache'
+import mkMemCache from '../mem-cache'
+
+test('`set` should set a value in the cache and `get` should return that value', t => {
+  const cache: Cache<string> = mkMemCache()
+  return cache.set('a', 'value')
+    .concatMap(() => cache.get('a'))
+    .do(cachedValue => t.is(cachedValue, 'value'))
+})
+test('`has` should return true if a value has previously been set', t => {
+  const cache: Cache<string> = mkMemCache()
+  return cache.set('a', 'value')
+    .concatMap(() => cache.has('a'))
+    .do(hasValue => t.true(hasValue))
+    .concatMap(() => cache.has('b'))
+    .do(hasValue => t.false(hasValue))
+})
+test('`delete` should remove a specific item from the cache', t => {
+  const cache: Cache<string> = mkMemCache()
+  return cache.set('a', 'value')
+    .concatMap(() => cache.delete('a'))
+    .concatMap(() => cache.has('a'))
+    .do(hasValue => t.false(hasValue))
+})
+test('`deletePattern` should remove all items matching a given pattern', t => {
+  const cache: Cache<string> = mkMemCache()
+  return O.forkJoin([
+    cache.set('a', 'value'),
+    cache.set('aa', 'value2'),
+    cache.set('b', 'value3'),
+  ])
+    .concatMap(() => cache.deletePattern(/a+/))
+    .concatMap(() => O.forkJoin([
+      cache.has('a'),
+      cache.has('aa'),
+      cache.has('b'),
+    ]))
+    .do(([a, aa, b]) => {
+      t.false(a)
+      t.false(aa)
+      t.true(b)
+    })
+})
+
+test('`clear` should remove all items in the cache', t => {
+  const cache: Cache<string> = mkMemCache()
+  return O.forkJoin([
+    cache.set('a', 'value'),
+    cache.set('aa', 'value2'),
+    cache.set('b', 'value3'),
+  ])
+    .concatMap(() => cache.clear())
+    .concatMap(() => O.forkJoin([
+      cache.has('a'),
+      cache.has('aa'),
+      cache.has('b'),
+    ]))
+    .do(([a, aa, b]) => {
+      t.false(a)
+      t.false(aa)
+      t.false(b)
+    })
+})
---
+++ src/mem-cache.js
@@ -0,0 +1,59 @@
+/**
+ * @flow
+ *
+ * TODO: evaluate saving items to LocalStorage to preserve memory. All
+ * response objects are in memory twice. That's kinda fucked. Writting to
+ * LocalStorage will allow me to delete the item in memory. It might even be
+ * feasible to store everything only in LocalStorage. Especially since most
+ * browsers don't handle interactions with LocalStorage as simple disk write/
+ * reads. Instead they keep objects in memory and keep disk writes to a
+ * minimum. That could have the effect, that all objects would be kept in
+ * memory. This has to be tested.
+ * TODO: Kap cache object size.
+ */
+
+import Rx, { Observable as O } from 'rxjs'
+
+const undefinedValueError = 'undefined may not be passed as a value to setItem.'
+
+const mkMemCache = <Value>() => {
+  let store = {}
+
+  const cache = {
+    delete: (key: string) => {
+      delete store[key]
+      return O.of(true)
+    },
+    deletePattern: (pattern: RegExp) =>
+      O.from(
+        Object.keys(store)
+          .filter(key => pattern.test(key))
+          .map(cache.delete)
+      )
+        .mergeAll()
+        .toArray(),
+    set: (key: string, value: Value) => {
+      if (value === undefined) {
+        return O.throw(new Error(undefinedValueError))
+      }
+      store[key] = value
+      return O.of(value)
+    },
+    has: (key: string) => {
+      const item = store[key]
+      return O.of(store.hasOwnProperty(key))
+    },
+    get: (key: string): Rx.Observable<Value|void> => {
+      return cache.has(key)
+        ? O.of(store[key])
+        : O.of(undefined)
+    },
+    clear: () => {
+      store = {}
+      return O.of(true)
+    }
+  }
+  return cache
+}
+
+export default mkMemCache
