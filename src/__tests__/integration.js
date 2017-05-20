adb39b695b72e926828d385c4b448408cb83318d
Luca Nils Schmid <me@lucaschmid.net>
2017-05-21 01:27:51 +0200

    feat: write integration tests

--- src/__tests__/integration.js
+++ src/__tests__/integration.js
@@ -1,6 +1,7 @@
 // @flow
 
 import test from 'ava'
+import { Observable as O } from 'rxjs'
 
 import { mockWorker } from './helpers'
 
@@ -12,8 +13,105 @@ import {
   mkRequestQueue,
 } from '../'
 
-const mainThread = mockWorker()
-const workerThread = mockWorker()
-mkInterface(mainThread)
+test('should call request function', t => {
+  const [main, worker] = mockWorker()
+  const requestMock = (url, options) => {
+    t.is(url, 'https://example.com')
+    return O.of(true)
+  }
 
-test.todo('')
+  const addRequest = mkRequestQueue(requestMock)
+  const cache = mkCachePolicy({})(mkMemCache())
+  mkReceiver(worker, addRequest, cache)
+  const orq = mkInterface(main)
+  return orq.addRequest('https://example.com')
+})
+
+test('should have a ttl cache', t => {
+  const [main, worker] = mockWorker()
+  let i = 0
+  const requestMock = (url, options) => {
+    ++i
+    return O.of(true)
+  }
+
+  const addRequest = mkRequestQueue(requestMock)
+  const cache = mkCachePolicy({ ttl: 100 })(mkMemCache())
+  mkReceiver(worker, addRequest, cache)
+  const orq = mkInterface(main)
+  return orq.addRequest('https://example.com')
+    .delay(1)
+    .concatMap(() => orq.addRequest('https://example.com'))
+    .do(() => { t.is(i, 1) })
+})
+
+test('should eliminate duplicate GET requests in queue', t => {
+  const [main, worker] = mockWorker()
+  let i = 0
+  const requestMock = (url, options) => {
+    ++i
+    return O.of(true).delay(50)
+  }
+
+  const addRequest = mkRequestQueue(requestMock)
+  const cache = mkCachePolicy({})(mkMemCache())
+  mkReceiver(worker, addRequest, cache)
+  const orq = mkInterface(main)
+  return orq.addRequest('https://example.com')
+    .concatMap(() => orq.addRequest('https://example.com'))
+    .do(() => { t.is(i, 1) })
+})
+
+test('should not eliminate duplicate non-GET requests in queue', t => {
+  const [main, worker] = mockWorker()
+  let i = 0
+  const requestMock = (url, options) => {
+    ++i
+    return O.of(true).delay(50)
+  }
+
+  const addRequest = mkRequestQueue(requestMock)
+  const cache = mkCachePolicy({})(mkMemCache())
+  mkReceiver(worker, addRequest, cache)
+  const orq = mkInterface(main)
+  return orq.addRequest('https://example.com', { method: 'POST' })
+    .concatMap(() => orq.addRequest('https://example.com', { method: 'POST' }))
+    .do(() => { t.is(i, 2) })
+})
+
+test('should not cache non-GET requests', t => {
+  const [main, worker] = mockWorker()
+  let i = 0
+  const requestMock = (url, options) => {
+    ++i
+    return O.of(true)
+  }
+
+  const addRequest = mkRequestQueue(requestMock)
+  const cache = mkCachePolicy({ ttl: 100 })(mkMemCache())
+  mkReceiver(worker, addRequest, cache)
+  const orq = mkInterface(main)
+  return orq.addRequest('https://example.com', { method: 'POST' })
+    .delay(1)
+    .concatMap(() => orq.addRequest('https://example.com', { method: 'POST' }))
+    .do(() => { t.is(i, 2) })
+})
+
+test.cb('should cancel unsubscribed requests', t => {
+  const [main, worker] = mockWorker()
+  let i = 0
+  const requestMock = (url, options) => O.create(() => () => {
+    t.pass()
+    t.end()
+  })
+
+  const addRequest = mkRequestQueue(requestMock)
+  const cache = mkCachePolicy({ ttl: 100 })(mkMemCache())
+  mkReceiver(worker, addRequest, cache)
+  const orq = mkInterface(main)
+  const sub = orq.addRequest('https://example.com', { method: 'POST', cancelable: true })
+    .subscribe()
+  setTimeout(() => {
+    sub.unsubscribe()
+  }, 10)
+})
