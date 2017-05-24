8c311535bd6484ce3d0a2892c02f2a46abb8a36d
Luca Nils Schmid <me@lucaschmid.net>
2017-05-24 19:18:42 +0200

    chore: expose Request type

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 // @flow
 
+import type { Request as RequestType } from './request'
+
 export { default as mkInterface } from './interface'
 export { default as mkReceiver } from './receiver'
 export { default as mkMemCache } from './mem-cache'
 export { default as mkCachePolicy } from './cache-policy'
 export { default as mkRequestQueue } from './request-queue'
+
+export type Request = RequestType
